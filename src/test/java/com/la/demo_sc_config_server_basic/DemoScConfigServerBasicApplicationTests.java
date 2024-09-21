09de5fe7c33eab106f76471915eacf53332f2c43
lsalvatierra <daplicacionesmoviles1@gmail.com>
2024-09-21 15:08:56 -0500

    initial commit

---
+++ src/test/java/com/la/demo_sc_config_server_basic/DemoScConfigServerBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.la.demo_sc_config_server_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoScConfigServerBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
